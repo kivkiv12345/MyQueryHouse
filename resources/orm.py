28b805a90e04d00f63601b530f998e3902f75b89
kivkiv12345 <kevincarlsen@live.dk>
2021-04-23 06:29:12 +0200

    Further ORM progress

--- resources/orm.py
+++ resources/orm.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     raise SystemExit("(⊙＿⊙') Wha!?... Are you trying to run orm.py?\n"
                      " You know this is a bad idea; right? You should run app.py instead :)")
 
-from typing import Type, Dict, List, Any, Tuple
+from typing import Type, Dict, List, Any, Tuple, Union
 from mysql.connector.connection import MySQLConnection
 from mysql.connector.cursor_cext import CMySQLCursor
 
@@ -86,13 +86,20 @@ class _DBModelMeta(type):
 class DBModel(metaclass=_DBModelMeta):
     """ Django'esque model class which converts table rows to Python class instances. """
 
-    fields: Dict[str, Any] = None
+    fields: Dict[str, Any] = None  # Holds the values of the instances.
+    model = None
 
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, *args, zipped_data: zip=None) -> None:
         """
         :param table_name: used to override the queried table name.
         """
-        print(kwargs)
+        # Allow a more readable way to access the class itself from its instances.
+        self.model: Type[DBModel] = self.__class__
+
+        data = zipped_data or zip((col[0] for col in self.Meta.column_data), *args)
+        datadict = {fieldname: value for fieldname, value in data}
+        self.fields.update(datadict)
+
         super().__init__()
 
     @property
@@ -101,9 +108,12 @@ class DBModel(metaclass=_DBModelMeta):
 
     class Meta:
         """ Holds information about the makeup of the current class. """
+        _col_hint = Union[str, None]
+
         table_name: str = None
         pk_column: str = None
         fields: Tuple[ModelField, ...] = None
+        column_data: Tuple[Tuple[_col_hint, bytes, _col_hint, _col_hint, _col_hint, _col_hint], ...]
 
     def __str__(self) -> str:
         return f"Meta for {self.Meta.table_name}"
@@ -121,7 +131,7 @@ def init_orm():
 
     def get_columns(table_name:str):
         CURSOR.execute(f"SHOW COLUMNS FROM {DATABASE_NAME}.{table_name}")
-        columndata = [column for column in CURSOR]
+        columndata = *(column for column in CURSOR),
         return columndata, next(col[0] for col in columndata if col[3] == 'PRI')
 
     for name in table_names:
@@ -132,7 +142,14 @@ def init_orm():
         model = type(name, (DBModel,), {'fields': {column[0]: None for column in columns}})
 
         # We add Meta to the model after declaration, such that it may refer back to its model.
-        setattr(model, 'Meta', type('Meta', (DBModel.Meta,), {'fields': tuple(ModelField(column) for column in columns), 'pk_column': pk_column, 'table_name': name}))
+        metadata = {
+            'fields': tuple(ModelField(column) for column in columns),
+            'pk_column': pk_column,
+            'table_name': name,
+            'column_data': columns,
+        }
+
+        setattr(model, 'Meta', type('Meta', (DBModel.Meta,), metadata))
 
         # Once we're finished with the current model, we add it to the dictionary with will hold them all.
         Models[name] = model  # We use typehinting to indicate attributes that cannot be inferred from our generic type.
